e47453f35131a97ec9b485c43f910c70a66636e7
silaakarapur <73967039+silaakarapur@users.noreply.github.com>
2020-11-26 00:03:57 +0300

    keelimeoyunu added.

---
+++ Kelime oyunu/Assets/Mainmenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mainmenü : MonoBehaviour
+{
+    [Header("baslangicsayfalari")]
+    public GameObject oyunekrani;
+    public GameObject seviyeekrani;
+    public GameObject Siralamaekrani;
+    public GameObject Ayarlarekrani;
+    
+    string level;
+    
+
+    
+    public void oyun()
+    {
+        //oyunu başlatan kısım
+      Application.LoadLevel("level");
+      {
+    
+      }
+    }
+    public void Seviye()
+    {
+        oyunekrani.SetActive(false);
+        seviyeekrani.SetActive(true);
+    }
+    public void Seviye2menu()
+    {
+        oyunekrani.SetActive(true);
+        seviyeekrani.SetActive(false);
+    }
+    public void Siralama()
+    {
+        oyunekrani.SetActive(false);
+        Siralamaekrani.SetActive(true);
+    }
+    public void Siralama2menu()
+    {
+        oyunekrani.SetActive(true);
+        Siralamaekrani.SetActive(false);
+    }
+    public void Ayarlar()
+    {
+        oyunekrani.SetActive(false);
+        Ayarlarekrani.SetActive(true);
+    }
+    public void Ayarlar2menü()
+    {
+        oyunekrani.SetActive(true);
+        Ayarlarekrani.SetActive(false);
+    }
+    public void cikis()
+    {
+        Application.Quit();
+
+    }
+}
