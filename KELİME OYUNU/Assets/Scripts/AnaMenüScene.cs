19f7b8a17891b68091f0a40380975c7203908035
silaakarapur <73967039+silaakarapur@users.noreply.github.com>
2020-12-09 20:11:59 +0300

    9.12.2020

---
+++ KELİME OYUNU/Assets/Scripts/AnaMenüScene.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class AnaMenüScene : MonoBehaviour
+{
+    [Header("LEVEL MENÜ HEADER")]
+    public GameObject LevelPanel;
+
+    [Header("ANA MENÜ HEADER")]
+    public  GameObject Anamenupaneli;
+
+    [Header("SETTİNG PANEL HEADER")]
+    public GameObject SettingPanel;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void levelpaneldenanamenuye(){
+        LevelPanel.SetActive(false);
+        Anamenupaneli.SetActive(true);
+    }
+    public void Settingdenanamenuye(){
+        SettingPanel.SetActive(false);
+        Anamenupaneli.SetActive(true);
+    }
+    public void LevelPanelegir(){
+        Anamenupaneli.SetActive(false);
+        LevelPanel.SetActive(true);
+    }
+    public void SettingPanelegir(){
+        Anamenupaneli.SetActive(false);
+        SettingPanel.SetActive(true);
+    }
+    
+    public void çıkış(){
+        Application.Quit();
+    }
+}
---
+++ KELİME OYUNU/Assets/Scripts/oyunbaşlama.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oyunbaşlama : MonoBehaviour
+{
+    public static string girilen = "";
+    public static bool ilktiklama = false;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+    public void tiklandi()
+    {
+        girilen +=GetComponent<TextMesh>().text;
+        Debug.Log(girilen);
+    }
+    public void OnMouseDown()
+    {
+        if (ilktiklama == false)
+        {
+            tiklandi();
+            ilktiklama = true;
+        }
+     
+    }
+    public void OnMouseEnter()
+    {
+        if (ilktiklama == true)
+        {
+            tiklandi();
+        }
+        Debug.Log("üzerine gelindi");
+    }
+    public void OnMouseUp()
+    {
+        Debug.Log("bıraktı");
+    }
+}
