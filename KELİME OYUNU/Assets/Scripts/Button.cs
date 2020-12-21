303e3f5a0f0ec8c47555da84d174fe15a0dd8743
silaakarapur <73967039+silaakarapur@users.noreply.github.com>
2020-12-21 22:35:16 +0300

    21.12.2020

---
+++ KELİME OYUNU/Assets/Scripts/Button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    OyunOynama yonet;
+    Image renk;
+
+    string harf;
+    bool harfverildi = false;
+
+    public bool yokol = false;
+
+    void Start()
+    {
+        yonet = GameObject.Find("OyunOynama").GetComponent<OyunOynama>();
+        renk = GetComponent<Image>();
+        harf = GameObject.name;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ KELİME OYUNU/Assets/Scripts/OyunOynama.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OyunOynama : MonoBehaviour
+{
+    
+    public string[] sozluk;
+    string kelime = null;
+    public Text puan_txt;
+    public bool tiklandi = false;
+    int puan = 0;
+    int bulunankelimesayisi=0;
+    List<GameObject> isaretlibutonlar;
+    void Start()
+    {
+        isaretlibutonlar = new List<GameObject>();
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetButtonDown("0"))
+        {
+            tiklandi = true;
+        }
+        if (Input.GetButtonUp("0"))
+        {
+            tiklandi = false;
+            yaziolustur();
+            puan_txt.text = puan.ToString();
+
+        }
+    }
+
+    void yaziolustur()
+    {
+        foreach(string kelimeler in sozluk)
+        {
+            if (kelimeler == kelime)
+            {
+                puan += 100;
+                bulunankelimesayisi++;
+                foreach(GameObject buton in isaretlibutonlar)
+                {
+                   
+                }
+            }
+        }
+    }
+}
